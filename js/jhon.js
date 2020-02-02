9fa9a08d73e832be62a4a8dc03b8c73cc043c8c2
Jhon Oquendo <losbarajaos@gmail.com>
2020-02-01 21:22:33 -0500

    Segundo avance - cambio de idioma

---
+++ js/jhon.js
@@ -0,0 +1,373 @@
+function cambiar_idioma(id){
+
+    var content = document.getElementById("contiene_todo");
+
+
+
+    var español = `
+
+<div class="top_header_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-5 col-sm-6">
+                <div class="top_social_bar">
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true" title="Facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true" title="Twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true" title="LinkedIn"></i></a>
+                </div>
+            </div>
+            <div class="col-7 col-sm-6">
+                <div class="signup-search-area d-flex align-items-center justify-content-end">
+                    <div class="search_button">
+                        <a class="searchBtn" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="search-hidden-form">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search-anything" placeholder="Buscar...">
+                            <input type="submit" value="" class="d-none">
+                            <span class="searchBtn"><i class="fa fa-times" aria-hidden="true"></i></span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<header class="header_area">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <div class="logo_area text-center">
+
+                    <img src="img/tuna_img/logo.PNG">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="text-center">
+                    <h3>MISIÓN</h3>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <p>Satisfacer las necesidades de nuestros consumidores con el objetivo de cumplir sus
+                        expectativas mediante un producto nutritivo conservando la receta original del fruto.</p>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="text-center">
+                    <h3>VISIÓN</h3>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <p>Ser líderes en el mercado internacional mediante la producción de almíbar de tunas y
+                        otras frutas, logrando aumentar la cuota de mercado.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</header>
+
+<section class="categories_area clearfix" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4" style="border-radius: 10px solid red; ">
+                <div class="single_catagory wow fadeInUp" data-wow-delay=".3s">
+                    <img src="img/tuna_img/tuna1.PNG" alt="">
+
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single_catagory wow fadeInUp" data-wow-delay=".6s">
+                    <img src="img/tuna_img/tuna2.PNG" alt="">
+
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="single_catagory wow fadeInUp" data-wow-delay=".9s">
+                    <img src="img/tuna_img/tuna3.jpg" alt="">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="row justify-content-center">
+    <div class="col-10">
+        <div class="text-center">
+            <h3>CONSERVAR DE TUNA EN ALMIBAR</h3>
+        </div>
+        <hr>
+        <div class="text-center">
+            <p>Fabricado  en  Perú, contenido  neto 250gr.  Ingradientes:   Tuna  maduras,   
+                carnosas, almibar, agua y azúcar información Nutricional: Porcion 20gr, 
+                valor energético: 46 kcal. Carbohidratos:11  gr.  Proteínas:  01 gr.  
+                Grasa  total:  0.1 gr. Grasa  saturadas: 0gr.  Grasas  transgénicas: 0gr. 
+                Fibra  alimentaria  0.3gr.  Sodio:  0mgr.  Valores  diarios  con  base  a  una  
+                dieta de 2000 kcal, sus valores diarios, pueden ser mayores o menores dependiendo 
+                de sus necesidades enegéticas.LOTE Y CONSUMIR PREFERENTEMENTE ANTES DE: ver en envase.
+                Una vez abierto.</p>
+        </div>
+    </div>
+</div>
+
+<section class="blog_area section_padding_0_80">
+    <div class="container">
+        <div class="row justify-content-center">
+
+            
+                <div class="col-8">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th colspan="2" class="text-center">TABLA NUTRIMENTAL</th>
+                            </tr>
+                            <tr>
+                                <th class="text-center">CONCEPTO</th>
+                                <th class="text-center">TUNA</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-center">Energía (Kcal.)</td>
+                                <td class="text-center">36.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Proteína (gr.)</td>
+                                <td class="text-center">1.30</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Grasa Total (gr.)</td>
+                                <td class="text-center">0.20</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Carbohidratos (gr.)</td>
+                                <td class="text-center">12.10</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Calcio (mg)</td>
+                                <td class="text-center">136.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Fósforo (mg)</td>
+                                <td class="text-center">20.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Zinc (mg)</td>
+                                <td class="text-center">0.26</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Potasio (mg)</td>
+                                <td class="text-center">143.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Hierro (mg)</td>
+                                <td class="text-center">0.17</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Vitamina C (mg)</td>
+                                <td class="text-center">33.66</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Tiamina (mg)</td>
+                                <td class="text-center">0.01</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Riboflavina (mg)</td>
+                                <td class="text-center">0.04</td>
+                            </tr>
+                            <tr>
+                                <td class="text-center">Niacina (mg)</td>
+                                <td class="text-center">0.42</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            
+            
+
+            <div class="row">
+                <div class="col-6">
+                    <div class="single-widget-area about-me-widget text-center">
+                        <div class="widget-title">
+                            <h6>Socio</h6>
+                        </div>
+                        <div class="about-me-widget-thumb">
+                            <img src="img/tuna_img/socio1.PNG" alt="">
+                        </div>
+                        <h4 class="font-shadow-into-light">Christian Manuel Llontop Guzman</h4>
+                        <p>Estudiante de Negocios Internacionales | Perú</p>
+                        <p></p>
+                    </div>
+                </div>
+                
+                <div class="col-6">
+                    <div class="single-widget-area about-me-widget text-center">
+                        <div class="widget-title">
+                            <h6>Socio</h6>
+                        </div>
+                        <div class="about-me-widget-thumb">
+                            <img src="img/tuna_img/socio2.jpg" alt="">
+                        </div>
+                        <h4 class="font-shadow-into-light">César Jeyson López Chacón</h4>
+                        <p>Estudiante de Negocios Internacionales | Perú</p>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+</section>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <hr>
+            <div class="text-center">
+                <h3>VALORES</h3>
+            </div>
+            <hr>
+            <div class="text-center">
+                    <ul>
+                        <li>El compromiso en asumir las actividades asignadas con responsabilidad y
+                            motivación.</li>
+                            <br>
+                        <li>Calidad en lograr la excelencia en la cadena de valor y promover la mejora
+                            continua.</li>
+                            <br>
+                        <li>Integridad en actuar de forma profesional y responsable en el ámbito social y
+                            ambiental.</li>
+                            <br>
+                        <li>Claridad en transmitir nuestros objetivos, ideas, percepciones y sugerencias
+                            de forma clara y conciso.</li>
+                            <br>
+                    </ul>
+                    <hr>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="text-center">
+                <h3>QUIÉNES SOMOS</h3>
+            </div>
+            <hr>
+            <div class="text-center">
+                <ul>
+                    <li>DELICIA EN ALMIBAR S.A.C empresa peruana está dedicada a la distribucion de
+                        productos alimenticios, principalmente frutas procesadas.</li>
+                        <br>
+                    <li>Su producto Principal “DELI TUNA”: Conservas de Tuna en Almíbar cuenta con un alto
+                        valor nutricional y las grandes cantidades de beneficios que trae consigo. El producto
+                        se exporta a los Estados Unidos y próximamente a otros mercados internacionales.</li>
+                        <br>
+                    <li>El Perú es uno de los países con la mayor variedad de frutas. Se calcula que
+                        aproximadamente existen 623, algunas de las cuales son desconocidas en el mundo,
+                        esta biodiversidad nos da una ventaja competitiva en comparación con otros países en
+                        los que se procesa fruta, ya que podemos ofrecer una mayor variedad de sabores y
+                        texturas.</li>
+                        <br>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="instargram_area owl-carousel section_padding_100_0 clearfix" id="portfolio">
+
+
+    <div class="instagram_gallery_item">
+
+        <img src="img/tuna_img/img1.PNG" alt="">
+
+        <div class="hover_overlay">
+            <div class="yummy-table">
+                <div class="yummy-table-cell">
+                    <div class="follow-me text-center">
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="instagram_gallery_item">
+        
+        <img src="img/tuna_img/img2.PNG" alt="">
+
+        <div class="hover_overlay">
+            <div class="yummy-table">
+                <div class="yummy-table-cell">
+                    <div class="follow-me text-center">
+                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Follow me</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+</div>
+
+<div class="social_icon_area clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="footer-social-area d-flex">
+                    <div class="single-icon">
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i><span>facebook</span></a>
+                    </div>
+                    <div class="single-icon">
+                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="footer_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="footer-content">
+                    <div class="footer-logo-area text-center">
+                        <img src="img/tuna_img/logo_final.PNG">
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="footer-content">
+                    <div class="footer-logo-area text-center">
+                        <img src="img/tuna_img/logo_final.PNG">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="copy_right_text text-center">
+                    <p>Copyright &copy; Todos los derechos reservados | DELICIA EN ALMÍBAR S.A.C 2020 </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>`;
+
+if(id=="1"){
+    location.href = 'index.html';
+}else if("2"){
+    content.innerHTML = español;
+}
+
+
+    //alert(id);//
+}
